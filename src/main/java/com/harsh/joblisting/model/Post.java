01ab6fbccb982b82554e4ada847d94248edf922e
harsh <harshmistryarm@gmail.com>
2025-01-17 17:49:54 +0530

    Changes

--- src/main/java/com/harsh/joblisting/model/Post.java
+++ src/main/java/com/harsh/joblisting/model/Post.java
@@ -1,5 +1,6 @@
 package com.harsh.joblisting.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "JobPost")
@@ -8,5 +9,6 @@ public class Post {
     private String profile;
     private String desc;
     private int exp;
+    @JsonIgnore
     private String tech[];
 }
